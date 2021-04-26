f401bb160d0cc61b212704c47cdd44445a1db658
Nahid <Nahidmahmud14@yahoo.com>
2021-04-26 17:19:59 -0400

    added css and javascript files

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+  ];
+  
